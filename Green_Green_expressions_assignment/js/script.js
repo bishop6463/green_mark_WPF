37db2d7bae6cbc410c1d349f5a2205bb94f78f17
mark green <info_wickedpixels@yahoo.com>
2015-02-11 22:17:58 -0500

    First commit for GTT

---
+++ Green_Green_expressions_assignment/js/script.js
@@ -0,0 +1,7 @@
+//mark.green.
+// sdi
+// section 2
+// week two work
+// first draft
+// add alert to test file
+alert("javascript works!");
---
+++ go2training2/js/script.js
@@ -0,0 +1,21 @@
+//mark.green.
+// sdi
+// section 2
+// week two work
+// first draft
+// add alert to test file
+alert("javascript works!");
+
+//variables and data types
+//var myAddress = "123 Elm Street";
+//var milesToStore = 15;
+//var isThereConstruction = true;
+var myFirstName;
+var myLastName;
+
+// prompts
+myFirstName = prompt("Please enter your first name: ", "Mark");
+myLastName = prompt("Please enter your last name: ", "Green");
+
+// outputs
+console.log("Your name is " + myFirstName + " " + myLastName + ".");
