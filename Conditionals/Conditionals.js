2c54bc2a3ffb222f9d382f593add1577023662a7
mark green <info_wickedpixels@yahoo.com>
2015-02-13 19:10:06 -0500

    first commit for week 3: conditionals

---
+++ Conditionals/Conditionals.js
@@ -0,0 +1,86 @@
+//condition
+//example
+
+
+
+//math expressions:
+// > greater than < less than >= greater than or equal to
+// <= less than or equal to != not equal == equal to
+// = is for assignment, putting a value into a variable
+// == is for comparing the values (comparitive operators)
+
+
+//sunny = true
+//temp = 82;
+//    goToTheBeach();
+//}else{
+ //   goToTheMovies()
+//}
+//if(temp >70){
+//    goToTheBeach();
+//}
+//nesting conditionals
+
+//if(sunny){
+//    goToTheBeach();
+
+//    if(warmWater){
+//    wearNewSuit();
+//}
+//}else{
+//    goToTheMovies();
+//}
+//conditional logic
+
+//var oldEnough = false; //boolean logic
+// if the child is old enough, print to the console "you can ride!'
+//if(oldEnough){
+//code performed if condition is true
+//    console.log("you can ride!");
+//}
+//console.log("what comes after?");
+
+//rational expressions
+
+//var kidHeight = 30;
+//var minHeight = 48;
+// if the child is over 48 inches "you can ride!'
+//if(kidHeight > 48) {
+//code performed if condition is true
+//    console.log("you can ride the coaster!")
+//}
+
+//conditional logic with an expression
+
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+
+
+// if the child is over 48 inches "you can ride!'
+//if(kidHeight + sneakerLift > minHeight) {//note the difference with the addition of sneakerLift
+//code performed if condition is true
+ //   console.log("you can ride the coaster!")
+//}
+
+// if and else
+
+if(kidHeight > minHeight){
+
+    console.log("you can ride the coaster");
+}
+if(kidHeight < minHeight){
+    console.log("sorry you have some growing to do");
+}
+
+
+
+
+
+
+
+
+
+
+
+
